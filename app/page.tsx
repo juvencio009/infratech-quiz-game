176ee569e8bf77794616ed2b1586cff615b38af4
Juvêncio Filipe Penga <labmin.jf@gmail.com>
2025-12-04 18:07:32 +0200

    Interface  De Espera

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 
+import Timer from './componets/timer';
+
 export default function Home() {
   return (
     <main
@@ -17,23 +19,31 @@ export default function Home() {
         padding: "1rem",
       }}
     >
-      <h1 style={{ fontSize: "3rem", marginBottom: "1rem" }}>
+      <h1 style={{ fontSize: "3rem", marginBottom: "1rem",fontFamily:"system-ui" }}>
         Quiz Game
       </h1>
-      <p style={{ fontSize: "1.2rem", maxWidth: "400px", marginBottom: "2rem" }}>
-        Bem-vindo ao jogo de quiz sobre programação! Clique no botão abaixo para começar.
+      <p style={{ fontSize: "1.2rem", maxWidth: "400px", marginBottom: "2rem",  }}>
+        Olá seja bem-vindo.
+        <span>Se voce está vendo está Página significa que o jogo ainda está no forno</span>
       </p>
+      <h2>Até o Lançamento</h2>
+      <div><Timer/></div>
+      <br />
       <button
         style={{
           padding: "1rem 2rem",
           fontSize: "1.2rem",
           borderRadius: "8px",
           border: "none",
-          cursor: "pointer",
-          backgroundColor: "#00d2ff",
-          color: "#000",
+          backgroundColor: "#638591ff",
+          pointerEvents:"none",
+          color:"#eee",
           fontWeight: "bold",
+          cursor:"pointer",
           transition: "all 0.2s ease-in-out",
+        }}
+        onClick={()=>{
+          alert("O jogo ainda está em desenvolvimento")
         }}
         onMouseEnter={(e) =>
           (e.currentTarget.style.backgroundColor = "#00bcd4")
